e46fc9f7c6c1639ac22667f617acf8c54e30804c
adam terlson <adam.terlson@gmail.com>
2014-04-22 18:36:40 -0500

    Removing sinon

--- test/common.js
+++ test/common.js
@@ -4,8 +4,5 @@ global.chai = require("chai");
 // expect style
 global.expect = require("chai").expect;
 
-// sinon
-global.sinon = require('sinon');
-
 // as promised
 require("mocha-as-promised")();
